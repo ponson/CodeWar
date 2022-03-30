b577a48bd1d3bc687f62b58513a4e31e701fe3ea
ponson <ponsonsun@gmail.com>
2022-03-30 11:15:48 +0800

    Completed and passed.

---
+++ 7kyuExes_and_Ohs.py
@@ -0,0 +1,11 @@
+def xo(s):
+    lower_str = s.lower()
+    count_x = lower_str.count('x')
+    count_o = lower_str.count('o')
+    if count_x == count_o:
+        return True
+    else:
+        return False
+
+print(xo("True expected"))
+print(xo("oTrue expected"))
