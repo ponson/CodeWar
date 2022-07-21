65fea4ce15bc518890255bfaeeba15357d9342cf
ponson <ponsonsun@gmail.com>
2022-07-21 16:48:46 +0800

    Finished the task.

---
+++ 4kyuSquareIntoSquares_master.py
@@ -0,0 +1,13 @@
+def decompose(n):
+    def _recurse(s, i):
+        if s < 0:
+            return None
+        if s == 0:
+            return []
+        for j in range(i-1, 0, -1):
+            sub = _recurse(s - j**2, j)
+            if sub != None:
+                return sub + [j]
+    return _recurse(n**2, n)
+
+print(decompose(12))
