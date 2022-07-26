da51c1c1b01d2d02dafbfcaf1341cfe365f44cf7
ponson <ponsonsun@gmail.com>
2022-07-26 16:11:43 +0800

    Use a very easy way to calculate the posibilities.

---
+++ ExerciseYourMathBrain/Quiz01.py
@@ -0,0 +1,16 @@
+N = 100
+cnt = 0
+
+for l in range(N+1):
+    for r in range(l, N+1):
+        if l > r - l:
+            if l != N - r:
+                cnt+=1
+        elif l < r - l:
+            if r - l != N - r:
+                cnt+=1
+        else:
+            if (l < N - r):
+                cnt+=1
+
+print(f"cnt={cnt}")
