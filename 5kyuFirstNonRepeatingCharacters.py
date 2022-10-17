914056680e847dc5e26eaf74ec9ae3d81b2e9642
ponson <ponsonsun@gmail.com>
2022-10-17 10:58:20 +0800

    Done.

--- 5kyuFirstNonRepeatingCharacters.py
+++ 5kyuFirstNonRepeatingCharacters.py
@@ -2,15 +2,28 @@ def first_non_repeating_letter(string):
     chars = []
     char_counts = []
     for c in string:
-        if c.upper() not in chars or c.lower() not in chars:
+        if c.upper() not in chars and c.lower() not in chars:
             chars.append(c)
             char_counts.append(1)
         else:
-            index = chars.index(c)
+            try:
+                index = chars.index(c)
+            except ValueError:
+                if c.isupper():
+                    index = chars.index(c.lower())
+                else:
+                    index = chars.index(c.upper())
             char_counts[index] += 1
 
-    idx = char_counts.index(1)
+    try:
+        idx = char_counts.index(1)
+    except ValueError:
+        return ""
     return chars[idx]
     
 
-print(first_non_repeating_letter("aabbccdeef"))
+# print(first_non_repeating_letter("aabbccdeef"))
+# print(first_non_repeating_letter("aabbccee"))
+# print(first_non_repeating_letter(""))
+print(first_non_repeating_letter("sTreSS"))
+print(first_non_repeating_letter("Go hang a salami, I\'m a lasagna hog!"))
