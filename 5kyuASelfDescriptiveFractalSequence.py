3c0ed09036d5d662b81824a88bf57eaee09e58d7
ponson <ponsonsun@gmail.com>
2022-11-23 16:29:50 +0800

    Finally, the 3rd version 0.0068 secs faster than the 2nd version 23 times. It passed the answer! Good Job! the code is shortest either.

--- 5kyuASelfDescriptiveFractalSequence.py
+++ 5kyuASelfDescriptiveFractalSequence.py
@@ -8,20 +8,15 @@ def check_rules(curlist, trimm, count):
 
 @timer
 def a112382(n):
-    output = [1]
-    up_trimm = []
+    output = [1] 
     new_num_start = 2
-    new_num_count = 1
-    for i in range(n):
-        num = check_rules(output, up_trimm, new_num_count)
-        if  num > 0:
-            output.append(num)
-            up_trimm.append(num)
-            new_num_count = 0
-        else:
+    trimm_idx = 0
+    while len(output) <= n:
+        output.append(output[trimm_idx])
+        trimm_idx += 1
+        for nc in range(output[trimm_idx]):
             output.append(new_num_start)
             new_num_start += 1
-            new_num_count += 1
 
     return output[n] 
 
