d8d47cea62e92a98f472da24dd77673d8f780948
ponson <ponsonsun@gmail.com>
2023-01-13 14:40:14 +0800

    Done. First version.

---
+++ 5kyuFlotsam.py
@@ -0,0 +1,250 @@
+
+#Okay
+pic1 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '-', '|', ' ', '|',
+        '-', '|', ' ', '|', '-', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', ' ', '|', ' ', '|',
+     ' ', '|', ' ', '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['~', '\\', '-', '-', '-', '-', '-', '-', '-', '|', '-', '|', '-', '|',
+        '-', '|', '-', '|', '-', '|', '-', '-', '-', '-', '-', '-', '/', '~'],
+    ['~', '~', '\\', ' ', ' ', ' ', 'F', ' ', ' ', ' ', '│', ' ', ' ', ' ',
+        'S', ' ', ' ', ' ', '│', ' ', ' ', ' ', 'T', ' ', ' ', '/', '~', '~'],
+    ['~', '~', '~', '\\', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-',
+        '-', '-', '-', '-', 'x', '-', '-', '-', '-', '-', '/', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+        '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+]
+
+#Almost Okay
+pic2 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '-', '|', ' ', '|',
+        '-', '|', ' ', '|', '-', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', ' ', '|', ' ', '|',
+     ' ', '|', ' ', '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['~', '\\', '-', '-', '-', '-', '-', '-', '-', '|', '-', '|', '-', '|',
+        '-', '|', '-', '|', '-', '|', '-', '-', '-', '-', '-', '-', '/', '~'],
+    ['~', '~', '\\', ' ', ' ', ' ', 'F', ' ', ' ', ' ', '│', ' ', ' ', ' ',
+        'S', ' ', ' ', ' ', '│', ' ', ' ', ' ', 'T', ' ', ' ', 'x', '~', '~'],
+    ['~', '~', '~', '\\', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-',
+        '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '/', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+        '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+]
+
+#More
+pic3 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '=', '=', '|', ' ', ' ', '|', '=',
+        '=', '|', ' ', ' ', '|', '=', '=', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', '_', '|', '_', '_', '|', '_', '_', '|', '_',
+     '_', '|', '_', '_', '|', '_', '_', '|', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', '_', '_', '|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+     '_', '_', '_', '_', '_', '_', '_', '_', '_', '|', '_', '_', '_', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', '_', '_', '|', '_', '_', '[', ']', '_', '_', '[', ']', '_', '_', '[', ']', '_',
+     '_', '[', ']', '_', '_', '[', ']', '_', '_', '[', ']', '_', '_', '|', '_', 'x', 'x', ' '],
+    ['~', '|', ' ', ' ', ' ', 'F', ' ', ' ', 'x', ' ', ' ', 'S', ' ', ' ', ' ', ' ', '|', ' ',
+     ' ', 'T', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '/', '~'],
+    ['~', 'x', '_', '_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_', 'x', '_', '|', '_',
+     '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '/', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+     '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+]
+
+#Enemy Inside
+pic4 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '=', '=', '|', ' ', ' ', '|', '=',
+        '=', '|', ' ', ' ', '|', '=', '=', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', '_', '|', '_', '_', '|', '_', '_', '|', '_',
+     '_', '|', '_', '_', '|', '_', '_', '|', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', '_', '_', '|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+     '_', '_', '_', '_', '_', '_', '_', '_', '_', '|', '_', '_', '_', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', '_', '_', '|', '_', '_', '[', ']', '_', '_', '[', ']', '_', '_', '[', ']', '_',
+     '_', '[', ']', '_', '_', '[', ']', '_', '_', '[', ']', '_', '_', '|', '_', 'x', 'x', 'x'],
+    ['~', '|', ' ', ' ', ' ', 'F', ' ', ' ', 'x', ' ', ' ', 'S', ' ', ' ', ' ', ' ', 'x', ' ',
+     ' ', 'T', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '/', '~'],
+    ['~', '|', '_', '_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_', '_', '_', '|', '_',
+     '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '/', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+     '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+]
+
+#No one
+pic5 = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '=', '=', '|', ' ', ' ', '|', '=',
+        '=', '|', ' ', ' ', '|', '=', '=', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', '_', '|', '_', '_', '|', '_', '_', '|', '_',
+     '_', '|', '_', '_', '|', '_', '_', '|', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', '_', '_', '|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+     '_', '_', '_', '_', '_', '_', '_', '_', '_', '|', '_', '_', '_', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', '_', '_', '|', '_', '_', '[', ']', '_', '_', '[', ']', '_', '_', '[', ']', '_',
+     '_', '[', ']', '_', '_', '[', ']', '_', '_', '[', ']', '_', '_', '|', '_', 'x', 'x', ' '],
+    ['~', '|', ' ', ' ', ' ', 'F', ' ', ' ', 'x', ' ', ' ', 'S', ' ', ' ', ' ', ' ', 'x', ' ',
+     ' ', 'T', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '/', '~'],
+    ['~', '|', 'x', '_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_', '_', '_', '|', '_',
+     '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '/', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~',
+     '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+]
+
+
+
+def check_water(ship, c_idx, r_idx):
+    r_len = len(ship)
+    c_len = len(ship[0])
+    check_list = [(r_idx, c_idx+1), (r_idx, c_idx-1), (r_idx+1, c_idx), (r_idx-1, c_idx)]
+    is_water = False
+    for px in check_list:
+        if 0 <= px[1] < r_len and 0 <= px[0] < c_len:
+            if ship[px[1]][px[0]] == '~':
+                is_water = True
+                break
+    
+    return is_water
+
+
+def flotsam(image):
+    survive = {'F':'Frank', 'S': 'Sam', 'T': 'Tom'}
+    for r in image:
+        for c in r:
+            print(c, end="")
+        print()
+
+    x_points = []
+    col_idx = 0
+    for row in image:
+        idx, offset = 0, 0
+        row_x_count = row.count('x')
+        for _ in range(row_x_count):
+            try:
+                idx = row[offset:].index('x')
+                if check_water(image, col_idx, idx + offset):
+                    x_points.append((col_idx, idx + offset))
+                    print(f"x is in ({col_idx}, {idx + offset}) ")
+                idx += 1
+                offset += idx
+            except ValueError:
+                pass
+
+        col_idx += 1
+
+    print(x_points)
+    
+    for x in x_points:
+        rIdx, cIdx = x[0], x[1]
+        #Right side
+        for right_idx in range(cIdx+1, len(image[0])):
+            if image[rIdx][right_idx] == ' ' or image[rIdx][right_idx] == 'x':
+                image[rIdx][right_idx] = '~'
+            elif image[rIdx][right_idx] in survive.keys():
+                survive.pop(image[rIdx][right_idx])
+                image[rIdx][right_idx] = '~'
+            else:
+                break
+        #Left side
+        for left_idx in range(cIdx-1, -1, -1):
+            if image[rIdx][left_idx] == ' ' or image[rIdx][left_idx] == 'x':
+                image[rIdx][left_idx] = '~'
+            elif image[rIdx][left_idx] in survive.keys():
+                survive.pop(image[rIdx][left_idx])
+                image[rIdx][left_idx] = '~'
+            else:
+                break
+        # Up side
+        for up_idx in range(rIdx-1, -1, -1):
+            if image[up_idx][cIdx] == ' ' or image[up_idx][cIdx] == 'x':
+                image[up_idx][cIdx] = '~'
+                #Right side
+                for right_idx in range(cIdx+1, len(image[0])):
+                    if image[up_idx][right_idx] == ' ' or image[up_idx][right_idx] == 'x':
+                        image[up_idx][right_idx] = '~'
+                    elif image[up_idx][right_idx] in survive.keys():
+                        survive.pop(image[up_idx][right_idx])
+                        image[up_idx][right_idx] = '~'
+                    else:
+                        break
+                #Left side
+                for left_idx in range(cIdx-1, -1, -1):
+                    if image[up_idx][left_idx] == ' ' or image[up_idx][left_idx] == 'x':
+                        image[up_idx][left_idx] = '~'
+                    elif image[up_idx][left_idx] in survive.keys():
+                        survive.pop(image[up_idx][left_idx])
+                        image[up_idx][left_idx] = '~'
+                    else:
+                        break
+            elif image[up_idx][cIdx] in survive.keys():
+                survive.pop(image[up_idx][cIdx])
+                image[up_idx][cIdx] = '~'
+                #Right side
+                for right_idx in range(cIdx+1, len(image[0])):
+                    if image[up_idx][right_idx] == ' ' or image[up_idx][right_idx] == 'x':
+                        image[up_idx][right_idx] = '~'
+                    elif image[up_idx][right_idx] in survive.keys():
+                        survive.pop(image[up_idx][right_idx])
+                        image[up_idx][right_idx] = '~'
+                    else:
+                        break
+                #Left side
+                for left_idx in range(cIdx-1, -1, -1):
+                    if image[up_idx][left_idx] == ' ' or image[up_idx][left_idx] == 'x':
+                        image[up_idx][left_idx] = '~'
+                    elif image[up_idx][left_idx] in survive.keys():
+                        survive.pop(image[up_idx][left_idx])
+                        image[up_idx][left_idx] = '~'
+                    else:
+                        break
+            else:
+                break
+
+        # Down side
+        for up_idx in range(rIdx+1, len(image)):
+            if image[up_idx][cIdx] == ' ' or image[up_idx][cIdx] == 'x':
+                image[up_idx][cIdx] = '~'
+                #Right side
+                for right_idx in range(cIdx+1, len(image[0])):
+                    if image[up_idx][right_idx] == ' ' or image[up_idx][right_idx] == 'x':
+                        image[up_idx][right_idx] = '~'
+                    elif image[up_idx][right_idx] in survive.keys():
+                        survive.pop(image[up_idx][right_idx])
+                        image[up_idx][right_idx] = '~'
+                    else:
+                        break
+                #Left side
+                for left_idx in range(cIdx-1, -1, -1):
+                    if image[up_idx][left_idx] == ' ' or image[up_idx][left_idx] == 'x':
+                        image[up_idx][left_idx] = '~'
+                    elif image[up_idx][left_idx] in survive.keys():
+                        survive.pop(image[up_idx][left_idx])
+                        image[up_idx][left_idx] = '~'
+                    else:
+                        break
+            elif image[up_idx][cIdx] in survive.keys():
+                survive.pop(image[up_idx][cIdx])
+                image[up_idx][cIdx] = '~'
+                #Right side
+                for right_idx in range(cIdx+1, len(image[0])):
+                    if image[up_idx][right_idx] == ' ' or image[up_idx][right_idx] == 'x':
+                        image[up_idx][right_idx] = '~'
+                    elif image[up_idx][right_idx] in survive.keys():
+                        survive.pop(image[up_idx][right_idx])
+                        image[up_idx][right_idx] = '~'
+                    else:
+                        break
+                #Left side
+                for left_idx in range(cIdx-1, -1, -1):
+                    if image[up_idx][left_idx] == ' ' or image[up_idx][left_idx] == 'x':
+                        image[up_idx][left_idx] = '~'
+                    elif image[up_idx][left_idx] in survive.keys():
+                        survive.pop(image[up_idx][left_idx])
+                        image[up_idx][left_idx] = '~'
+                    else:
+                        break
+            else:
+                break
+    print(f"Survior is {survive.values()}")
+    return " ".join(survive.values())
+
+
+tests = [pic1, pic2, pic3, pic4, pic5]
+#tests = [pic5]
+for t in tests:
+    print(flotsam(t))
+    print([i for i, j in locals().items() if j == t][0])
