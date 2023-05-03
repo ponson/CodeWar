ace7f599dd2d4af56fe79c2a9cfbd6ac615f0d67
ponson <ponsonsun@gmail.com>
2023-05-03 16:29:13 +0800

    Final version of the quiz.

--- 4kyuFindTheUnknownDigit.py
+++ 4kyuFindTheUnknownDigit.py
@@ -1,55 +1,16 @@
-def replaceQmarkAndCheckValid(num, n):
-    num = num.replace('?', n)
-
-    if (int(num) == 0 and num != '0') or (num[0] == '0' and len(num) > 1) or ():
-        return num, "Invalid"
-
-    if len(num) > 1 and (num[0] == '0' or (num[0] == '-' and num[1] == '0')):
-        return num, "Invalid"
-        
-    return num, "Valid"
-
+import re
 
 def solve_runes(runes):
-    #TODO 1 切出tokens。 operators, operands, answer
-    operands = ['*', '+', '-']
-    equal_idx = runes.find('=')
-    answer = runes[equal_idx+1:]
-    op_idx = 0
-    optor = ''
-    for idx, c in enumerate(runes):
-        if c in operands:
-            if idx == 0 and c == '-':
-                continue
-            optor = c
-            op_idx = idx
-            break
-    
-    op1 = runes[:op_idx]
-    op2 = runes[op_idx+1:equal_idx]
-    print(f"op1={op1}, op2={op2}, optor={optor} answer={answer}")
-    #TODO 2 除去現有的數字, 一次將未出現的數字去替換?執行運算和Answer做比對，找出第一個符合的lowest，找不到return -1
-    num_pool = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for item in runes:
-        if item in num_pool:
-            num_pool.remove(item)
-
-    print(num_pool)
-    for n in num_pool:
-        new_op1, result = replaceQmarkAndCheckValid(op1, n)
-        if result == "Invalid":
+    for i in range(10):
+        if str(i) in runes:
             continue
-        new_op2, result = replaceQmarkAndCheckValid(op2, n)
-        if result == "Invalid":
-            continue
-        new_answer, result = replaceQmarkAndCheckValid(answer, n)
-        if result == "Invalid":
-            continue
-
-        if int(new_answer) == eval(new_op1+optor+new_op2):
-            return int(n)
         else:
-            continue
+            candidate = runes.replace('?', str(i))
+            if re.search(r"([^\d]|\b)0\d+", candidate):
+                continue
+            items = candidate.split("=")
+            if int(items[1]) == eval(items[0]):
+                return i
 
     return -1
     
